468e8ab0d5e7ecbcb42fc95f2dbb58b50a8f2e32
Sam Falberg <samuelfalberg@gmail.com>
2018-07-20 16:04:01 -0500

    Api key function

--- static/script.js
+++ static/script.js
@@ -1,6 +1,14 @@
 var api_key = "75f35a7eec548de064e93a2cdbf3d29d"
 
-<<<<<<< HEAD
+function queryGiphy(query, resultCallback) {
+  var charity_url = "https://api.data.charitynavigator.org/v2"
+                  + "api_key=" + api_key
+                  + "&q=" + query
+                  + "&limit=" + 1
+  jQuery.get(charity_url, resultCallback)
+}
+
+
 
 =======
 // Application ID
