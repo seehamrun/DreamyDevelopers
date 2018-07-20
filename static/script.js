deecc7580e14920ae55775270eda28a75d397838
mariam <mariam.tolba1@gmail.com>
2018-07-20 15:24:58 -0500

    mt

---
+++ static/script.js
@@ -0,0 +1,10 @@
+var api_key = ""
+
+// function queryCharity(query, resultCallback) {
+//   // var giphy_url = "http://api.giphy.com/v1/gifs/search?"
+//   //                 + "api_key=" + giphy_api_key
+//   //                 + "&q=" + query
+//   //                 + "&limit=" + 1
+//   // somehow reformat this so it corresponds with the charity navigator api
+//   jQuery.get(giphy_url, resultCallback)
+// }
