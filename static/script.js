fc8498d3d730ac86bf26e9860e741e5a2d60e757
Walter <uruchimaw@gmail.com>
2018-07-20 15:27:16 -0500

    Added "Value" key

--- static/script.js
+++ static/script.js
@@ -1,5 +1,9 @@
 var api_key = "75f35a7eec548de064e93a2cdbf3d29d"
 
+// Application ID
+// This is the application ID, like a login ID for your application. You should send with each API request.
+// Value: 71c6533f
+
 // function queryCharity(query, resultCallback) {
 //   // var giphy_url = "http://api.giphy.com/v1/gifs/search?"
 //   //                 + "api_key=" + giphy_api_key
