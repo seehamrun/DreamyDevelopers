ec3179b61488598d6d029d5c724e75eb0ed25bc4
Sam Falberg <samuelfalberg@gmail.com>
2018-07-23 10:37:49 -0500

    app_key changed to api_key

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 function queryCharity(query, resultCallback) {
   var charity_url = 'https://api.data.charitynavigator.org/v2/Organizations?app_id=71c6533f'
-                  + '&app_key=' + app_key
+                  + '&app_key=' + api_key
                   + '&search=' + query
                   + '&rated=true'
   jQuery.get(charity_url, resultCallback)
