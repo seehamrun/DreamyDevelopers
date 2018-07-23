eb0980b31ed822ba9b6128b9b4dfb7a4a8b86b2e
Sam Falberg <samuelfalberg@gmail.com>
2018-07-23 09:43:17 -0500

    api stuff

--- static/script.js
+++ static/script.js
@@ -2,14 +2,14 @@ var api_key = "75f35a7eec548de064e93a2cdbf3d29d"
 
 
 function queryCharity(query, resultCallback) {
-  var charity_url = "https://api.data.charitynavigator.org/v2"
-                  + "api_key=" + api_key
-                  + "&q=" + query
-                  + "&limit=" + 1
+  var charity_url = 'https://api.data.charitynavigator.org/v2/Organizations?app_id=71c6533f&app_key=75f35a7eec548de064e93a2cdbf3d29d&search=' + query
+                  + '&rated=true'
   jQuery.get(charity_url, resultCallback)
 }
+// var charity_url = 'https://api.data.charitynavigator.org/v2/Organizations?app_id=71c6533f&app_key=75f35a7eec548de064e93a2cdbf3d29d&search=' + query
+// + '&rated=true'
 
-console.log(queryCharity("hunger"))
+//console.log(queryCharity())
 console.log('hey hi hello')
 
 
