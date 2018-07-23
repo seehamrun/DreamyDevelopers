3e2e76c31499384edfe398a887db1f385ea4d419
Sam Falberg <samuelfalberg@gmail.com>
2018-07-23 14:58:08 -0500

    name changes for response_html

--- main.py
+++ main.py
@@ -31,20 +31,20 @@ class MainPageHandler(webapp2.RequestHandler):
 class AboutUsHandler(webapp2.RequestHandler):
     def get (self):
         self.response.headers['Content-Type'] = 'text/html'
-        response2_html = jinja_env.get_template('templates/aboutus.html')
-        self.response.write(response2_html.render())
+        response_html = jinja_env.get_template('templates/aboutus.html')
+        self.response.write(response_html.render())
 
 class SearchHandler(webapp2.RequestHandler):
     def get (self):
         self.response.headers['Content-Type'] = 'text/html'
-        response3_html = jinja_env.get_template('templates/search.html')
-        self.response.write(response3_html.render())
+        response_html = jinja_env.get_template('templates/search.html')
+        self.response.write(response_html.render())
 
 class ResultsHandler(webapp2.RequestHandler):
     def get (self):
         self.response.headers['Content-Type'] = 'text/html'
-        response3_html = jinja_env.get_template('templates/results.html')
-        self.response.write(response3_html.render())
+        response_html = jinja_env.get_template('templates/results.html')
+        self.response.write(response_html.render())
 
 
 app = webapp2.WSGIApplication([
