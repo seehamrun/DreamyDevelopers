ab5e4cbe353ee2503c3f4c92f9f1bba9834549d3
mariam <mariam.tolba1@gmail.com>
2018-07-23 09:48:16 -0500

    moving api key -mt

--- static/script.js
+++ static/script.js
@@ -1,8 +1,5 @@
-var api_key = "75f35a7eec548de064e93a2cdbf3d29d"
-
-
 function queryCharity(query, resultCallback) {
-  var charity_url = "https://api.data.charitynavigator.org/v2"
+  var charity_url = "http://api.charitynavigator.org/api/v1/search/?app_key=APP_KEY&app_id=APP_ID&field=value"
                   + "api_key=" + api_key
                   + "&q=" + query
                   + "&limit=" + 1
