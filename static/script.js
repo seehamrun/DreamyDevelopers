48a0aa229f62cbafb0f990fda190bd14696b6f87
Walter <uruchimaw@gmail.com>
2018-07-23 16:40:24 -0500

    Revert "Merge branch 'master' of https://github.com/seehamrun/DreamyDevelopers"
    This reverts commit 5ba1aa2207b4f6bffd274e5177154536b4047bcc, reversing
    changes made to 1753d1818a3a24af7691345b9253e6004e95ec4a.

--- static/script.js
+++ static/script.js
@@ -6,41 +6,21 @@ function queryCharity(query, resultCallback) {
   jQuery.get(charity_url, resultCallback)
 }
 
-function submitClick() {
-  console.log('hi')
-  var inputBox = document.querySelector('#queryBox')
-  var userInput = inputBox.value
-  queryCharity(userInput, function(data) {
-    //var resultstr = "";
-    var div = ""
-    for (var i = 0; i < data.length; i++){
-      console.log(data[i].mission)
-      console.log(data[i].charityName)
-      console.log(data[i].irsClassification.deductibility)
-      //resultstr = resultstr + "<p>'" + missionStatement + ' ' + charityName + ' ' + irsClassification + "'</p>"
-      var resultName = "<h1>" + data[i].charityName + "</h1>"
-      var resultMission = "<p>" + data[i].mission + "</p>"
-      var resultClassification = "<p>" + data[i].irsClassification.deductibility + "</p>"
-      div = div + "<div>" + resultName + resultMission + resultClassification + "</div>"
-    }
 
-    var resultDiv = document.querySelector('#result')
-    resultDiv.innerHTML = div
-  })
-}
+queryCharity("autism", function(data) {
+  var resultstr = "";
+  var div = ""
+  for (var i = 0; i < data.length; i++){
+    console.log(data[i].mission)
+    console.log(data[i].charityName)
+    console.log(data[i].irsClassification.deductibility)
+    //resultstr = resultstr + "<p>'" + missionStatement + ' ' + charityName + ' ' + irsClassification + "'</p>"
+    var resultName = "<h1>" + data[i].charityName + "</h1>"
+    var resultMission = "<p>" + data[i].mission + "</p>"
+    var resultClassification = "<p>" + data[i].irsClassification.deductibility + "</p>"
+    div = div + "<div>" + resultName + resultMission + resultClassification + "</div>"
+  }
 
-window.addEventListener('load', () => {
-  document.querySelector('#submit').addEventListener("click", submitClick)
-  console.log('hello')
-});
-
-
-function submitClick() {
-  var inputBox = document.querySelector('#searchBox')
-  var userInput = inputBox.value
-  queryCharity(userInput, displayResult)
-}
-
-window.addEventListener('load', () => {
-  document.querySelector('#submit').addEventListener("click", submitClick)
+  var resultDiv = document.querySelector('#result')
+  resultDiv.innerHTML = div
 });
