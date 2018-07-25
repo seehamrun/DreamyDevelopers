d39f6ad8dc8f45bf3a04417e5ee397b7e1e06d71
Sam Falberg <samuelfalberg@gmail.com>
2018-07-25 13:31:03 -0500

    News API works + CSS styling

--- static/details.js
+++ static/details.js
@@ -10,6 +10,7 @@ function charityNews(charity, resultCallback) {
   var news_url = 'https://newsapi.org/v2/everything?'
           + 'q=' + charity
           + '&sortBy=relevancy'
+          + '&language=en'
           + '&apiKey=' + news_api_key
   jQuery.get(news_url, resultCallback)
 }
@@ -18,19 +19,25 @@ function charityNews(charity, resultCallback) {
 charityDetails(einLookup, function(data) {
   var name = document.querySelector('#charityName')
   name.innerHTML = data.charityName
-  var deductibility = document.querySelector('#deductibility')
-  deductibility.innerHTML = data.irsClassification.deductibility
   var website = document.querySelector('#website')
   website.innerHTML = data.websiteURL
   website.href = data.websiteURL
+  var deductibility = document.querySelector('#deductibility')
+  deductibility.innerHTML = data.irsClassification.deductibility
+  var number = document.querySelector('#number')
+  var regularNumber = data.phoneNumber
+  var fixedNumber = regularNumber.slice(0, 3) + '-' + regularNumber.slice(3, 6) + '-' + regularNumber.slice(6)
+  number.innerHTML = fixedNumber
+  var address = document.querySelector('#address')
+  var mail = data.mailingAddress
+  address.innerHTML = mail.streetAddress1 + ', ' + mail.city + ', ' + mail.stateOrProvince + ' ' + mail.postalCode
   var cause = document.querySelector('#cause')
   cause.innerHTML = data.mission
   charityNews(data.charityName, function(data) {
-    console.log(data)
     var articles = data.articles
     var div = ""
     for (var i = 0; i < articles.length; i++){
-      var resultTitle = "<h3>" + articles[i].title + "</h3>"
+      var resultTitle = "<h3><a href=\"" + articles[i].url + "\">"+ articles[i].title + "</a></h3>"
       var resultDate = "<h5>" + articles[i].publishedAt.slice(0, 10) + "</h5>"
       var resultDescription = "<p>" + articles[i].description + "</p>"
       div = div + "<article>" + resultTitle + resultDate + resultDescription + "</article>"
